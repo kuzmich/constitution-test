26128fda9bdf8e3e7e6baf75c0ec3e3581729681
Alexey Kuzminich <akuzminich@gmail.com>
2022-08-15 18:52:34 +0800

    Добавил 3 часть анкеты - интеллект

---
+++ results-calculator.js
@@ -0,0 +1,33 @@
+class ResultsCalculator {
+
+    constructor(sectionID) {
+        let sectionInputs = Array.from(document.querySelectorAll(`${sectionID} input`));
+
+        this.vataInputs = sectionInputs.filter(input => input.name.includes('vata'));
+        this.pittaInputs = sectionInputs.filter(input => input.name.includes('pitta'));
+        this.kaphaInputs = sectionInputs.filter(input => input.name.includes('kapha'));
+
+        this.result = document.querySelector(`${sectionID} output`);
+
+        for (let input of sectionInputs) {
+            input.addEventListener('change', this.calcResults.bind(this));
+        }
+    }
+
+    isChecked(input) {
+        return input.checked;
+    }
+
+    calcResults() {
+        let vata = Math.round(this.vataInputs.filter(this.isChecked).length / this.vataInputs.length * 100);
+        let pitta = Math.round(this.pittaInputs.filter(this.isChecked).length / this.pittaInputs.length * 100);
+        let kapha = Math.round(this.kaphaInputs.filter(this.isChecked).length / this.kaphaInputs.length * 100);
+
+        let resultTemplate = `Вата ${vata}%, Питта ${pitta}%, Капха ${kapha}%.`;
+        this.result.textContent = resultTemplate;
+    }
+}
+
+new ResultsCalculator('#prakriti');
+new ResultsCalculator('#vikriti');
+new ResultsCalculator('#intelligence');
