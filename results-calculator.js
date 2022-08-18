2b95fd5c24fc5126610db4b00a34dd16daaa0436
Alexey Kuzminich <akuzminich@gmail.com>
2022-08-18 12:20:26 +0800

    Добавил финальную часть "Подведение итогов"

--- results-calculator.js
+++ results-calculator.js
@@ -1,6 +1,11 @@
 class ResultsCalculator {
 
     constructor(sectionID) {
+        this.vata = 0;
+        this.pitta = 0;
+        this.kapha = 0;
+
+        this.sectionID = sectionID;
         let sectionInputs = Array.from(document.querySelectorAll(`${sectionID} input`));
 
         this.vataInputs = sectionInputs.filter(input => input.name.includes('vata'));
@@ -23,12 +28,55 @@ class ResultsCalculator {
         let pitta = Math.round(this.pittaInputs.filter(this.isChecked).length / this.pittaInputs.length * 100);
         let kapha = Math.round(this.kaphaInputs.filter(this.isChecked).length / this.kaphaInputs.length * 100);
 
+        this.vata = vata;
+        this.pitta = pitta;
+        this.kapha = kapha;
+
         let resultTemplate = `<strong>Итог:</strong> Вата ${vata}%, Питта ${pitta}%, Капха ${kapha}%.`;
         this.result.innerHTML = resultTemplate;
+
+        let cells = document.querySelectorAll(`${this.sectionID}-results td`);
+        cells[1].textContent = `${vata}%`;
+        cells[2].textContent = `${pitta}%`;
+        cells[3].textContent = `${kapha}%`;
     }
 }
 
-new ResultsCalculator('#prakriti');
-new ResultsCalculator('#vikriti');
-new ResultsCalculator('#intelligence');
-new ResultsCalculator('#emotions');
+class SummaryTotalCalculator {
+    constructor(sectionCalculators) {
+        this.sectionCalculators = sectionCalculators;
+
+        let formInputs = document.querySelectorAll('form input');
+        for (let input of formInputs) {
+            input.addEventListener('change', this.calcResults.bind(this));
+        }
+    }
+
+    calcResults() {
+        let vata = 0;
+        let pitta = 0;
+        let kapha = 0;
+
+        for (let calculator of this.sectionCalculators) {
+            vata = vata + calculator.vata;
+            pitta = pitta + calculator.pitta;
+            kapha = kapha + calculator.kapha;
+        }
+
+        vata = Math.round(vata / this.sectionCalculators.length);
+        pitta = Math.round(pitta / this.sectionCalculators.length);
+        kapha = Math.round(kapha / this.sectionCalculators.length);
+
+        let cells = document.querySelectorAll('#summary-totals td');
+        cells[1].textContent = `${vata}%`;
+        cells[2].textContent = `${pitta}%`;
+        cells[3].textContent = `${kapha}%`;
+    }
+}
+
+let prakritiCalc = new ResultsCalculator('#prakriti');
+let vikritiCalc = new ResultsCalculator('#vikriti');
+let intelligenceCalc = new ResultsCalculator('#intelligence');
+let emotionsCalc = new ResultsCalculator('#emotions');
+
+new SummaryTotalCalculator([vikritiCalc, intelligenceCalc, emotionsCalc]);
